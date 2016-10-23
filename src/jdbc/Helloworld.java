507178ed22bf25851d32d397da5c0806669a2bc3
zzy <zzy_xpy@163.com>
2016-10-23 10:10:25 +0800

    finish PMD

--- src/jdbc/Helloworld.java
+++ src/jdbc/Helloworld.java
@@ -15,56 +15,61 @@ public class Helloworld {
     * char
     */
     public static void derivative(final String exp, final char flag) {
-        String mm = "";
-        int ii = -1;
-        int l, x, y, j, k, s;
-        l = exp.length();
-        x = 0;
-        while (ii < l - 1) {
-            ii++;
-            if ((exp.charAt(ii) == '+') || (ii == l - 1)) {
-                if (ii == l - 1) {
-                    y = ii;
+        String strMM = "";
+        int iter = -1;
+        int lll;
+        int xxx;
+        int yyy;
+        int jjj;
+        int kkk;
+        int sss;
+        lll = exp.length();
+        xxx = 0;
+        while (iter < lll - 1) {
+            iter++;
+            if ((exp.charAt(iter) == '+') || (iter == lll - 1)) {
+                if (iter == lll - 1) {
+                    yyy = iter;
                 } else {
-                    y = ii - 1;
+                    yyy = iter - 1;
                 }
-                k = 0;
-                s = 1;
+                kkk = 0;
+                sss = 1;
                 // System.out.println(x);
                 // System.out.println(y);
-                for (j = x; j <= y; j++) {
-                    if (exp.charAt(j) == flag) {
-                        k++;
+                for (jjj = xxx; jjj <= yyy; jjj++) {
+                    if (exp.charAt(jjj) == flag) {
+                        kkk++;
                     }
                 }
                 // System.out.println(k);
-                s = k;
-                if (k > 0) {
+                sss = kkk;
+                if (kkk > 0) {
                     int pd = 0;
-                    for (j = x; j <= y; j++) {
-                        if ((exp.charAt(j) == flag)
+                    for (jjj = xxx; jjj <= yyy; jjj++) {
+                        if ((exp.charAt(jjj) == flag)
                                 && (pd == 0)) {
                             pd = 1;
-                            mm = mm
-                            + String.valueOf(s);
+                            strMM = strMM
+                            + String.valueOf(sss);
                         } else {
-                            mm = mm + exp.charAt(j);
+                            strMM = strMM + exp.charAt(jjj);
                         }
                     }
                 }
-                x = y + 1;
+                xxx = yyy + 1;
             }
 
         }
-        for (ii = 0; ii < mm.length(); ii++) {
-            if ((!((ii == mm.length() - 1)
-                    && (mm.charAt(ii) == '+')))
-                    && (!((ii == 0)
-                    && (mm.charAt(ii) == '+')))) {
-                System.out.print(mm.charAt(ii));
+        for (iter = 0; iter < strMM.length(); iter++) {
+            if (!(iter == strMM.length() - 1
+                    && strMM.charAt(iter) == '+')
+                    && !(iter == 0
+                    && strMM.charAt(iter) == '+')) {
+                System.out.print(strMM.charAt(iter));
             }
         }
-        if (mm.length() == 0) {
+        if (strMM.length() == 0) {
             System.out.print(0);
         }
         System.out.println();
@@ -81,24 +86,24 @@ public class Helloworld {
     public static void main(final String[] args) {
         Scanner in = new Scanner(System.in);
         // 标志，避免使用while true
-        int x = 0;
-        String a = new String();
+        int flag = 0;
+        String newStr = new String();
         // 死循环
-        while (x == 0) {
+        while (flag == 0) {
             // n用来保存输入的字符串
-            String n = in.nextLine();
+            String str = in.nextLine();
             // 输入的不是命令
-            if (n.charAt(0) != '!') {
-                a = n;
-                pd(a);
+            if (str.charAt(0) != '!') {
+                newStr = str;
+                pd(newStr);
             } else {
                 // 化简命令，a为表达式，n为表达式
-                if (n.charAt(1) == 's') {
-                    simplify(a, n);
-                } else if (n.charAt(1) == 'd') {
-                    derivative(a, n.charAt(4));
+                if (str.charAt(1) == 's') {
+                    simplify(newStr, str);
+                } else if (str.charAt(1) == 'd') {
+                    derivative(newStr, str.charAt(4));
                 } else {
-                    x = 1;
+                    flag = 1;
                 }
             }
         }
@@ -113,47 +118,47 @@ public class Helloworld {
      */
     public static void pd(final String exp) {
         int flag = 0;
-        int l = exp.length();
+        int len = exp.length();
         int index = 0;
         // 遍历字符串,x=1不合法
-        for (int i = 0; i < l; i++) {
+        for (int i = 0; i < len; i++) {
             char ch = exp.charAt(i);
             // 是数字
-            if ((ch >= '0') && (ch <= '9')) {
+            if (ch >= '0' && ch <= '9') {
                 index++;
             } else {
                 index = 0;
             }
-            if ((index > 1) && (exp.charAt(i - index + 1) == '0')) {
+            if (index > 1 && exp.charAt(i - index + 1) == '0') {
                 flag = 1;
             }
-            if ((!(((ch >= '0') && (ch <= '9'))
-                    || ((ch >= 'a') && (ch <= 'z'))))
-                    && (ch != '*') && (ch != '+')) {
+            if ((!((ch >= '0' && ch <= '9')
+                    || (ch >= 'a' && ch <= 'z')))
+                    && ch != '*' && ch != '+') {
                 flag = 1;
             }
             if (i > 0) {
                 char c = exp.charAt(i - 1);
-                if (((ch >= 'a') && (ch <= 'z'))
-                        && ((c >= 'a')
-                        && (c <= 'z'))) {
+                if ((ch >= 'a' && ch <= 'z')
+                        && (c >= 'a'
+                        && c <= 'z')) {
                     flag = 1;
                 }
-                if (((ch == '+') || (ch == '*'))
-                        && ((c == '+') || (c == '*'))) {
+                if ((ch == '+' || ch == '*')
+                        && (c == '+' || c == '*')) {
                     flag = 1;
                 }
-                if (((ch >= 'a') && (ch <= 'z'))
-                        && ((c >= '0') && (c <= '9'))) {
+                if ((ch >= 'a' && ch <= 'z')
+                        && (c >= '0' && c <= '9')) {
                     flag = 1;
                 }
-                if (((c >= 'a') && (c <= 'z'))
-                        && ((ch >= '0') && (ch <= '9'))) {
+                if ((c >= 'a' && c <= 'z')
+                        && (ch >= '0' && ch <= '9')) {
                     flag = 1;
                 }
             }
-            if ((i == 0) || (i == l - 1)) {
-                if ((ch == '+') || (ch == '*')) {
+            if (i == 0 || i == len - 1) {
+                if (ch == '+' || ch == '*') {
                     flag = 1;
                 }
             }
@@ -167,62 +172,62 @@ public class Helloworld {
 
     /**
      * simplify.
-     * @param n
+     * @param exp
      *            string
-     * @param a
+     * @param exp2
      *              string
      */
-    public static void simplify(final String n, final String a) {
-        int[] d;
-        d = new int[27];
-        int l = n.length();
-        int l2 = a.length();
-        int i;
-        for (i = 1; i <= 26; i++) {
-            d[i] = 0;
+    public static void simplify(final String exp, final String exp2) {
+        int[] nums;
+        nums = new int[27];
+        int len = exp.length();
+        int len2 = exp2.length();
+        int iter;
+        for (iter = 1; iter <= 26; iter++) {
+            nums[iter] = 0;
         }
-        for (i = 0; i < l; i++) {
-            char m = n.charAt(i);
+        for (iter = 0; iter < len; iter++) {
+            char m = exp.charAt(iter);
             if ((m >= 'a') && (m <= 'z')) {
-                d[m - 96] = 1;
+                nums[m - 96] = 1;
             }
         }
-        i = 9;
-        while (i < l2 - 1) {
-            i++;
-            char c = a.charAt(i);
-            if ((c >= 'a') && (c <= 'z')) {
+        iter = 9;
+        while (iter < len2 - 1) {
+            iter++;
+            char ch = exp2.charAt(iter);
+            if ((ch >= 'a') && (ch <= 'z')) {
                 int x;
-                x = i + 2;
-                int y;
-                y = x;
-                while (y < l2) {
-                    if ((a.charAt(y) >= '0')
-                        && (a.charAt(y) <= '9')) {
-                        y++;
+                x = iter + 2;
+                int yyy;
+                yyy = x;
+                while (yyy < len2) {
+                    if (exp2.charAt(yyy) >= '0'
+                        && exp2.charAt(yyy) <= '9') {
+                        yyy++;
                     } else {
                         break;
                     }
                 }
-                y--;
-                int k = 1;
-                int s = 0;
-                while (y - x >= 0) {
-                    int e = 0;
-                    e = a.charAt(y) - 48;
-                    s = s + e * k;
-                    y--;
-                    k = k * 10;
+                yyy--;
+                int kkk = 1;
+                int sss = 0;
+                while (yyy - x >= 0) {
+                    int eee = 0;
+                    eee = exp2.charAt(yyy) - 48;
+                    sss = sss + eee * kkk;
+                    yyy--;
+                    kkk = kkk * 10;
                 }
-                d[a.charAt(x - 2) - 96] = -s;
+                nums[exp2.charAt(x - 2) - 96] = -sss;
             }
         }
-        for (i = 0; i < l; i++) {
-            char c = n.charAt(i);
-            if (((c >= 'a') && (c <= 'z')) && (d[c - 96] < 0)) {
-                System.out.print(-d[c - 96]);
+        for (iter = 0; iter < len; iter++) {
+            char ch1 = exp.charAt(iter);
+            if (ch1 >= 'a' && ch1 <= 'z' && nums[ch1 - 96] < 0) {
+                System.out.print(-nums[ch1 - 96]);
             } else {
-                System.out.print(c);
+                System.out.print(ch1);
             }
         }
         System.out.println();
